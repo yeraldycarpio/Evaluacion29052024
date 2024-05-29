6704b802beb4e61c60f509557aed838279b21cb1
yeraldycarpio <2430933@gmail.com>
2024-05-29 16:22:04 -0600

    Clases

--- src/App.java
+++ src/App.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Empleado> productos = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Ingrese el ID del Empleado:");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Ingrese su nombre:");
+            String nombre = scanner.nextLine();
+
+            System.out.println("Ingrese su apellido:");
+            String apellido = scanner.nextLine();
+
+            System.out.println("Departamento en que vive:");
+            double departamento = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("Ingrese su salario:");
+            double salario = scanner.nextDouble();
+            scanner.nextLine();
+
+            Empleado empleado = new empleado (id, nombre, apellido, departamento, salario);
+            productos.add(empleado);
+
+           System.out.println("¿Quiere ingresar un Empleado más? (s/n):");
+            String respuesta = scanner.nextLine();
+            if (respuesta.equalsIgnoreCase("n")) {
+                continuar = false;
+            }
+        }
+
+        Collections.sort(empleado, Comparator.comparingInt(Empleado::getId));
+
+        Iterator<Empleado> iterator = empleado.iterator();
+         while (iterator.hasNext()) {
+            Empleado empleado = iterator.next();
+            System.out.println(empleado);
+        }
+
+        scanner.close();
     }
 }
--- src/Empleado.java
+++ src/Empleado.java
@@ -1,3 +1,48 @@
 public class Empleado {
+private int id;
+private String Nombre;
+private String Apellido;
+private String Departamento;
+private double Salario;
+public Empleado() {
+}
+public Empleado(int id, String nombre, String apellido, String departamento, double salario) {
+    this.id = id;
+    Nombre = nombre;
+    Apellido = apellido;
+    Departamento = departamento;
+    Salario = salario;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getNombre() {
+    return Nombre;
+}
+public void setNombre(String nombre) {
+    Nombre = nombre;
+}
+public String getApellido() {
+    return Apellido;
+}
+public void setApellido(String apellido) {
+    Apellido = apellido;
+}
+public String getDepartamento() {
+    return Departamento;
+}
+public void setDepartamento(String departamento) {
+    Departamento = departamento;
+}
+public double getSalario() {
+    return Salario;
+}
+public void setSalario(double salario) {
+    Salario = salario;
+}
 
 }
+
